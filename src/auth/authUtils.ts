c1a7cd518d0295d07fd53723888e5cd32f9da0cd
veerags <veerags2002@gmail.com>
2025-08-07 23:34:32 +0530

    commit

--- src/auth/authUtils.ts
+++ src/auth/authUtils.ts
@@ -5,11 +5,12 @@ import jwt from 'jsonwebtoken';
 export class AuthUtils {
     static tokengenerate = async ({ id, email, accountid, fullname }: { id: string, email: string, accountid: string, fullname: string }) => {
         const mongodb = await getMongodb()
+    const JWT_SECRET:any = process.env['JWT_SECRET'] || 'taskapp_secret_key';
 
         const payload = {
             id, fullname, email, accountid
         }
-        const token = jwt.sign(payload, 'prodect_secret_key', { expiresIn: '24h' });
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' });
 
         await mongodb.collection('Jwttoken').insertOne({ token, accountid, email, fullname, id })
 
